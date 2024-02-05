6a3da23c86a3e0ef01092c77d183a9fbe8c5ec3a
Алексей <aleksej.sirotenkov@simaster.ru>
2024-02-05 18:38:47 +0300

    Add serifs for hours divisions

--- clock/widget.cpp
+++ clock/widget.cpp
@@ -25,5 +25,13 @@ void Widget::paintEvent(QPaintEvent *event)
     QPainter painter(this);
     painter.drawEllipse(left, top, d, d);
 
+    // Засечки для часовых делений
+    for (std::size_t i = 0; i < 12; ++i) {
+        auto const degrees = (360.0 / 12) * i;
+        auto const radians = qDegreesToRadians(degrees);
+        auto x1 = x0 + r * qCos(radians), y1 = y0 + r *qSin(radians);
+        auto x2 = x0 + (r * 0.9) * qCos(radians), y2 = y0 + (r * 0.9) * qSin(radians);
+        painter.drawLine(x1, y1, x2, y2);
+    }
 
 }
