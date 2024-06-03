69311a17e775e81438beedd3650dcf5da59e6e63
Alex <aleksej.sirotenkov@simaster.ru>
2024-06-03 09:09:47 +0300

    Added drawing

--- clock/widget.cpp
+++ clock/widget.cpp
@@ -1,6 +1,8 @@
 #include <QPainter>
 #include <QtMath>
 #include <QDateTime>
+#include <QMouseEvent>
+#include <QColor>
 #include "widget.h"
 
 Widget::Widget(QWidget *parent)
@@ -38,23 +40,40 @@ void Widget::paintEvent(QPaintEvent *event)
     auto const left = (w - d) / 2, top = (h - d) / 2;
 
     QPainter painter(this);
-    painter.drawEllipse(left, top, d, d);
 
-    // Засечки для часовых делений
-    for (std::size_t i = 0; i < 12; ++i) {
-        auto const degrees = (360.0 / 12) * i;
-        auto const radians = qDegreesToRadians(degrees);
-        auto x1 = x0 + r * qCos(radians), y1 = y0 + r *qSin(radians);
-        auto x2 = x0 + (r * 0.9) * qCos(radians), y2 = y0 + (r * 0.9) * qSin(radians);
-        painter.drawLine(x1, y1, x2, y2);
+    QColor const active (Qt::gray);
+    QColor const inactive (Qt::lightGray);
+
+    painter.setPen(QPen(inactive, 5, Qt::SolidLine));
+
+    if (m_pressed) {
+    painter.setBrush(Qt::black); painter.drawEllipse(left, top, d, d);
+    } else {
+        painter.setBrush(Qt::white); painter.drawEllipse(left, top, d, d);
     }
+    painter.setRenderHint(QPainter::Antialiasing, true);
 
     // Засечки для минутных делений
-    for (std::size_t i = 0; i < 60; ++i) {
+    for (int i = 0; i < 60; ++i) {
         auto const degrees = (360.0 / 60) * i;
         auto const radians = qDegreesToRadians(degrees);
         auto x1 = x0 + r * qCos(radians), y1 = y0 + r *qSin(radians);
         auto x2 = x0 + (r * 0.95) * qCos(radians), y2 = y0 + (r * 0.95) * qSin(radians);
+
+        if (m_pressed) {
+            painter.setPen(QColor(active)); painter.drawLine(x1, y1, x2, y2);
+        } else {
+            painter.setPen(QColor(inactive)); painter.drawLine(x1, y1, x2, y2);
+        }
+    }
+
+    // Засечки для часовых делений
+    for (int i = 0; i < 12; ++i) {
+        auto const degrees = (360.0 / 12) * i;
+        auto const radians = qDegreesToRadians(degrees);
+        auto x1 = x0 + r * qCos(radians), y1 = y0 + r *qSin(radians);
+        auto x2 = x0 + (r * 0.9) * qCos(radians), y2 = y0 + (r * 0.9) * qSin(radians);
+        painter.setPen(QColor(255, 0, 0));
         painter.drawLine(x1, y1, x2, y2);
     }
 
@@ -65,6 +84,7 @@ void Widget::paintEvent(QPaintEvent *event)
         auto const radians = qDegreesToRadians(degrees - 90);
         auto x1 = x0, y1 = y0;
         auto x2 = x0 + (r * 0.5) * qCos(radians), y2 = y0 + (r * 0.5) * qSin(radians);
+        painter.setPen(QColor(71, 106, 125));
         painter.drawLine(x1, y1, x2, y2);
     }
 
@@ -73,6 +93,7 @@ void Widget::paintEvent(QPaintEvent *event)
         auto const radians = qDegreesToRadians(degrees - 90);
         auto x1 = x0, y1 = y0;
         auto x2 = x0 + (r * 0.7) * qCos(radians), y2 = y0 + (r * 0.7) * qSin(radians);
+        painter.setPen(Qt::green);
         painter.drawLine(x1, y1, x2, y2);
     }
 
@@ -81,7 +102,24 @@ void Widget::paintEvent(QPaintEvent *event)
         auto const radians = qDegreesToRadians(degrees - 90);
         auto x1 = x0, y1 = y0;
         auto x2 = x0 + (r * 0.8) * qCos(radians), y2 = y0 + (r * 0.8) * qSin(radians);
+        painter.setPen(Qt::yellow);
         painter.drawLine(x1, y1, x2, y2);
     }
+}
 
+void Widget::mousePressEvent(QMouseEvent* event)
+{
+    if (event->button() == Qt::MouseButton::LeftButton) {
+        m_pressed = true;
+        update();
+    }
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent* event) {
+    if (event->button() == Qt::MouseButton::LeftButton) {
+        m_pressed = false;
+        update();
+
+        emit clicked();
+    }
 }
--- clock/widget.h
+++ clock/widget.h
@@ -3,10 +3,14 @@
 
 #include <QWidget>
 #include <QTimer>
+#include <QString>
 
 class Widget : public QWidget
 {
     Q_OBJECT
+
+signals:
+    void clicked();
 
 public:
     Widget(QWidget *parent = nullptr);
@@ -18,9 +22,15 @@ private slots:
     void onTimeout();
 
 private:
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseReleaseEvent(QMouseEvent *event);
+
     QTimer timer_;
     int hours_ {0};
     int minutes_ {0};
     int seconds_ {0};
+
+    bool m_pressed = false;
+    bool m_hovered = false;
 };
 #endif // WIDGET_H
